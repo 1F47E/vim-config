f679f76b9040db11f611cf6f7abd2ad1112301b3
kas <32327548+kaspar1ndustries@users.noreply.github.com>
2023-01-12 14:54:35 +0800

    telescope refactoring + TODO

--- init.lua
+++ init.lua
@@ -20,7 +20,113 @@ require('packer').startup(function(use)
   use 'nvim-tree/nvim-web-devicons'
   use 'theprimeagen/harpoon'
   use 'mbbill/undotree'
-  use 'tpope.io/vim/fugitive'
+  -- use 'tpope.io/vim/fugitive'
+  use 'tpope/vim-fugitive'
+
+  -- Trouble
+  use {
+  "folke/trouble.nvim",
+  requires = "kyazdani42/nvim-web-devicons",
+  config = function()
+    require("trouble").setup {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    }
+  end
+}
+
+-- TODO 
+use {
+  "folke/todo-comments.nvim",
+  requires = "nvim-lua/plenary.nvim",
+  config = function()
+    require("todo-comments").setup {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+          signs = true, -- show icons in the signs column
+          sign_priority = 8, -- sign priority
+          -- keywords recognized as todo comments
+          keywords = {
+            FIX = {
+              icon = " ", -- icon used for the sign, and in search results
+              color = "error", -- can be a hex color, or a named color (see below)
+              alt = { "FIXME", "BUG", "FIXIT", "ISSUE" }, -- a set of other keywords that all map to this FIX keywords
+              -- signs = false, -- configure signs for some keywords individually
+            },
+            TODO = { icon = " ", color = "info" },
+            HACK = { icon = " ", color = "warning" },
+            WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
+            -- WTF = { icon = "🦄 ", color = "warning", alt = { "WARNING", "XXX" } },
+            WTF = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
+            PERF = { icon = " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
+            NOTE = { icon = " ", color = "hint", alt = { "INFO" } },
+            TEST = { icon = "⏲ ", color = "test", alt = { "TESTING", "PASSED", "FAILED" } },
+          },
+          gui_style = {
+            fg = "NONE", -- The gui style to use for the fg highlight group.
+            bg = "BOLD", -- The gui style to use for the bg highlight group.
+          },
+          merge_keywords = true, -- when true, custom keywords will be merged with the defaults
+          -- highlighting of the line containing the todo comment
+          -- * before: highlights before the keyword (typically comment characters)
+          -- * keyword: highlights of the keyword
+          -- * after: highlights after the keyword (todo text)
+          highlight = {
+            multiline = true, -- enable multine todo comments
+            multiline_pattern = "^.", -- lua pattern to match the next multiline from the start of the matched keyword
+            multiline_context = 10, -- extra lines that will be re-evaluated when changing a line
+            before = "", -- "fg" or "bg" or empty
+            keyword = "wide", -- "fg", "bg", "wide", "wide_bg", "wide_fg" or empty. (wide and wide_bg is the same as bg, but will also highlight surrounding characters, wide_fg acts accordingly but with fg)
+            after = "fg", -- "fg" or "bg" or empty
+            pattern = [[.*<(KEYWORDS)\s*:]], -- pattern or table of patterns, used for highlightng (vim regex)
+            comments_only = true, -- uses treesitter to match keywords in comments only
+            max_line_len = 400, -- ignore lines longer than this
+            exclude = {}, -- list of file types to exclude highlighting
+          },
+          -- list of named colors where we try to extract the guifg from the
+          -- list of highlight groups or use the hex color if hl not found as a fallback
+          colors = {
+            error = { "DiagnosticError", "ErrorMsg", "#DC2626" },
+            warning = { "DiagnosticWarn", "WarningMsg", "#FBBF24" },
+            info = { "DiagnosticInfo", "#2563EB" },
+            hint = { "DiagnosticHint", "#10B981" },
+            default = { "Identifier", "#7C3AED" },
+            test = { "Identifier", "#FF00FF" }
+          },
+          search = {
+            command = "rg",
+            args = {
+              "--color=never",
+              "--no-heading",
+              "--with-filename",
+              "--line-number",
+              "--column",
+            },
+            -- regex that will be used to match keywords.
+            -- don't replace the (KEYWORDS) placeholder
+            pattern = [[\b(KEYWORDS):]], -- ripgrep regex
+            -- pattern = [[\b(KEYWORDS)\b]], -- match without the extra colon. You'll likely get false positives
+          },
+        }
+
+  end
+}
+
+
+-- AUTOSAVE
+--   use({
+-- 	"Pocco81/auto-save.nvim",
+-- 	config = function()
+-- 		 require("auto-save").setup {
+--                 delay = 2050, -- auto save every 150 milliseconds (default)
+--         delay_write = 2050, -- auto write if idle for more than 150 milliseconds (default)
+-- 	debounce_delay = 3000
+-- 		 }
+-- 	end,
+-- })
+--
 
 
   use { -- LSP Configuration & Plugins
@@ -56,7 +162,7 @@ require('packer').startup(function(use)
   }
 
   -- Git related plugins
-  use 'tpope/vim-fugitive'
+  -- use 'tpope/vim-fugitive'
   use 'tpope/vim-rhubarb'
   use 'lewis6991/gitsigns.nvim'
 
@@ -213,7 +319,8 @@ require('telescope').setup {
       vertical = { width = 0.6 }
       -- other layout configuration here
     },
-    sorting_strategy = "ascending"
+    sorting_strategy = "ascending",
+    file_ignore_patterns = {"node_modules"} 
   },
 }
 
@@ -231,11 +338,14 @@ vim.keymap.set('n', '<leader>/', function()
   })
 end, { desc = '[/] Fuzzily search in current buffer]' })
 
-vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
-vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
-vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
-vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
-vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
+vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
+vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = '[S]earch [B]uffers' })
+
 
 -- [[ Configure Treesitter ]]
 -- See `:help nvim-treesitter`
@@ -345,6 +455,9 @@ local on_attach = function(_, bufnr)
     print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
   end, '[W]orkspace [L]ist Folders')
 
+  vim.o.updatetime = 250
+  vim.cmd [[autocmd! CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]]
+
   -- Create a command `:Format` local to the LSP buffer
   vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
     vim.lsp.buf.format()
@@ -412,14 +525,14 @@ cmp.setup {
     end,
   },
   mapping = cmp.mapping.preset.insert {
-    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<CR>'] = cmp.mapping.confirm {
-      behavior = cmp.ConfirmBehavior.Replace,
-      select = false,
-    },
-    -- ['<Tab>'] = cmp.mapping(function(fallback)
+    -- ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+    -- ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    -- ['<C-Space>'] = cmp.mapping.complete(),
+    -- ['<CR>'] = cmp.mapping.confirm {
+    --   behavior = cmp.ConfirmBehavior.Replace,
+    --   select = false,
+    -- },
+    -- ['<C-j>'] = cmp.mapping(function(fallback)
     --   if cmp.visible() then
     --     cmp.select_next_item()
     --   elseif luasnip.expand_or_jumpable() then
@@ -428,7 +541,7 @@ cmp.setup {
     --     fallback()
     --   end
     -- end, { 'i', 's' }),
-    -- ['<S-Tab>'] = cmp.mapping(function(fallback)
+    -- ['<C-k>'] = cmp.mapping(function(fallback)
     --   if cmp.visible() then
     --     cmp.select_prev_item()
     --   elseif luasnip.jumpable(-1) then
@@ -437,6 +550,12 @@ cmp.setup {
     --     fallback()
     --   end
     -- end, { 'i', 's' }),
+
+      ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+      ['<C-f>'] = cmp.mapping.scroll_docs(4),
+      ['<C-Space>'] = cmp.mapping.complete(),
+      ['<C-e>'] = cmp.mapping.abort(),
+      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
   },
   sources = {
     { name = 'nvim_lsp' },
@@ -467,8 +586,11 @@ vim.api.nvim_set_keymap('n', 'tt', ':NERDTreeToggle<CR>', {})
 --vim.api.nvim_set_keymap('n', '<C-f>', ':NERDTreeFind<CR>', {})
 vim.api.nvim_set_keymap('n', 'tm', ':NERDTreeMirror<CR>', {})
 -- Open nerdtree by default (lua)
--- TODO
+--
 
+
+-- TODO comments
+vim.api.nvim_set_keymap('n', '<leader>tt', ':TodoTelescope<CR>', {})
 --nvim.command("autocmd StdinReadPre * let s:std_in=1")
 --
 -- Open the existing NERDTree on each new tab.
@@ -478,7 +600,10 @@ vim.api.nvim_set_keymap('n', 'tm', ':NERDTreeMirror<CR>', {})
 vim.cmd[[colorscheme tokyonight]]
 
 -- Basic nav config
-vim.opt.guicursor = ""
+vim.opt.autoread = true
+vim.opt.autowrite = true
+
+-- vim.opt.guicursor = ""
 vim.opt.nu = true
 vim.opt.relativenumber = true
 vim.opt.tabstop = 4
@@ -508,13 +633,14 @@ vim.opt.termguicolors = true
 vim.g.mapleader = " "
 
 -- REMAPS
-
+--
+-- vim.keymap.set('n', '<C-w>', ':w<CR>', opts) -- save
 vim.keymap.set('v', 'J', ':m \'>+1<CR>gv=gv', opts)
 vim.keymap.set('v', 'K', ':m \'<-2<CR>gv=gv', opts)
 vim.keymap.set('n', 'Y', 'yg$', opts)
 vim.keymap.set('n', 'J', 'mzJ`z', opts)
 vim.keymap.set('n', '<C-d>', '<C-d>zz', opts)
-vim.keymap.set('n', '<C-u>', '<C-u>zz', { noremap = true, silent = true })
+vim.keymap.set('n', '<C-u>', '<C-u>zz', opts)
 vim.keymap.set('n', 'n', 'nzzzv', opts)
 vim.keymap.set('n', 'N', 'Nzzzv', opts)
 -- paste and throw away replaced place to null buffer
